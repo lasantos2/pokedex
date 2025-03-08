61ca4dfdf1f4b8458a60a7c59105a75f5158f2b7
AppleTruffle <appletrufflemuffinkinz@gmail.com>
2025-03-07 18:05:31 -0700

    starting cache along with tests

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -1,2 +1,49 @@
 package pokecache
 
+import(
+	"time"
+	"sync"
+)
+
+type Cache struct {
+	cache map[string]cacheEntry
+	mux *sync.Mutex
+}
+
+type cacheEntry struct {
+	createdAt time.Time
+	val []bye
+}
+
+func NewCache(interval time.Duration) Cache {
+	_cache := Cache{
+		cache:make(map[string]cacheEntry),
+		mux: &sync.Mutex{}
+	}
+
+	// call reaploop in new job
+	go _cache.reapLoop(interval)
+
+	return _cache
+}
+
+func (_cache *Cache) Add(key string, value []byte) {
+	_cache.mux.Lock()
+	defer _cache.mux.Unlock()
+	_cache.cache[key] = CacheEntry{
+		createdAt: time.Now().UTC(),
+		val: value,
+	}
+}
+
+func (_cache *Cache) Get(key string) ([]byte, bool) {
+	_cache.mux.Lock()
+	defer _cache.mux.Unlock()
+	val, ok := _cache.cache[key]
+	return val.val, ok
+}
+
+
+func (_cache *Cache) reapLoop(interval time.Duration) {
+
+}
