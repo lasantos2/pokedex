09040b9fa918ce1b5dbf92add7283530440cefb7
AppleTruffle <appletrufflemuffinkinz@gmail.com>
2025-03-28 19:09:49 -0600

    location list and required structure to get pokemon list

--- command_explore.go
+++ command_explore.go
@@ -1,11 +1,28 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+)
+
 //list all pokemon in the location given
-func commandExplore(cfg *config, locationName string) error {
+func commandExplore(cfg *config, args ...string) error {
+	if len(args) != 1 {
+		return errors.New("you must provide a location name")
+	}
 
+	name := args[0]
+	location, err := cfg.pokeapiClient.GetLocation(name)
+	if err != nil {
+		return err
+	}
 
-
-
+	fmt.Printf("Exploring %s...\n", location.Name)
+	fmt.Printf("Found Pokemon: \n")
+	for _, enc := range location.PokemonEncounters {
+		fmt.Printf(" - %s\n", enc.Pokemon.Name)
+	}
 
 	return nil
+
 }
