a76c6961686390de68b07eb9d71fb5032a7f3523
AppleTruffle <appletrufflemuffinkinz@gmail.com>
2025-04-07 14:45:35 -0600

    Inspect done with formatting

--- command_inspect.go
+++ command_inspect.go
@@ -5,6 +5,28 @@ import (
 	"fmt"
 )
 
+/*
+Stats []struct {
+		BaseStat int `json:"base_stat"`
+		Effort   int `json:"effort"`
+		Stat     struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"stat"`
+	} `json:"stats"`
+
+
+Types []struct {
+		Slot int `json:"slot"`
+		Type struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"type"`
+	} `json:"types"`
+
+
+*/
+
 //fetch information from pokedex and display some information
 func commandInspect(cfg *config, args ...string) error {
 	if len(args) != 1 {
@@ -24,6 +46,15 @@ func commandInspect(cfg *config, args ...string) error {
 	fmt.Println("Height:", pokeInfo.Height)
 	fmt.Println("Weight:", pokeInfo.Weight)
 	fmt.Println("Stats:")
+
+	for _, val := range pokeInfo.Stats {
+		fmt.Printf(" -%s: %d\n", val.Stat.Name, val.BaseStat)
+	}
+
+	fmt.Println("Types:")
+	for _, val := range pokeInfo.Types {
+		fmt.Printf(" -%s\n", val.Type.Name)
+	}
 
 
 
